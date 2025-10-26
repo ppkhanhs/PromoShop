fff3eb0dc6bfbec9eb6eb9529aa2fbb346204756
LuuKiett <luukie0401@gmail.com>
2025-10-26 14:16:32 +0700

    edit header and sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -2,21 +2,31 @@
     <a href="{{ route('admin.dashboard') }}"
        class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.dashboard') ? 'active fw-semibold text-primary' : 'text-dark' }}">
         <i class="fa-solid fa-chart-pie"></i>
-        Tổng quan
+        Trang tổng quan
     </a>
     <a href="{{ route('admin.promotions.index') }}"
        class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.promotions.*') ? 'active fw-semibold text-primary' : 'text-dark' }}">
         <i class="fa-solid fa-gift"></i>
-        Khuyến mãi
+        Quản lý khuyến mãi
+    </a>
+    <a href="#"
+       class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.promotions.*') ? 'active fw-semibold text-primary' : 'text-dark' }}">
+        <i class="fa-solid fa-ticket"></i>
+        Quản lý mã giảm giá
+    </a>
+    <a href="#"
+       class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.promotions.*') ? 'active fw-semibold text-primary' : 'text-dark' }}">
+        <i class="fa-solid fa-ticket"></i>
+        Điều kiện khuyến mãi
     </a>
     <a href="{{ route('admin.products.index') }}"
        class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.products.*') ? 'active fw-semibold text-primary' : 'text-dark' }}">
         <i class="fa-solid fa-box"></i>
-        Sản phẩm
+        Quản lý sản phẩm
     </a>
     <a href="{{ route('admin.users.index') }}"
        class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.users.*') ? 'active fw-semibold text-primary' : 'text-dark' }}">
         <i class="fa-solid fa-user-group"></i>
-        Người dùng
+        Quản lý người dùng
     </a>
 </nav>
